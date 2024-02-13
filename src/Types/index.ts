104cb6b0751041e937f3be329ca29d58058e12ce
selly361 <seikou615@gmail.com>
2024-02-13 07:33:10 +0000

    Add: types

---
+++ src/Types/index.ts
@@ -0,0 +1,34 @@
+export interface Feedback {
+	_id?: string
+	title: string
+	detail: string
+	likes: {
+		count: number
+		users: string[]
+	}
+    tutorial: string
+	dislikes: {
+		count: number
+		users: string[]
+	}
+	comments: Comment[]
+}
+
+export interface Comment {
+	_id?: string
+	username: string
+	text: string
+	replies: Reply[]
+}
+
+export interface Reply {
+	_id?: string
+	username: string
+	text: string
+}
+
+export interface Tutorial {
+	_id?: string
+	name: string
+    lesson: string
+}
