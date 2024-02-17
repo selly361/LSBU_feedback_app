0e80a4ccc4231680cba1c1c8ded19b88e51d6556
selly361 <seikou615@gmail.com>
2024-02-17 14:59:35 +0000

    Add: `index.ts` file to export svgs as `ReactComponent`

---
+++ src/Assets/Icons/index.ts
@@ -0,0 +1,10 @@
+export { ReactComponent as ArrowDownIcon } from "./icon-arrow-down.svg";
+export { ReactComponent as ArrowLeftIcon } from "./icon-arrow-left.svg";
+export { ReactComponent as ArrowUpIcon } from "./icon-arrow-up.svg";
+export { ReactComponent as CheckIcon } from "./icon-check.svg";
+export { ReactComponent as CommentsIcon } from "./icon-comments.svg";
+export { ReactComponent as NewFeedbackIcon } from "./icon-new-feedback.svg";
+export { ReactComponent as SuggestionsIcon } from "./icon-suggestions.svg";
+export { ReactComponent as EmptyIllustration } from "./illustration-empty.svg";
+export { ReactComponent as ConfusedFace } from "./confused-face.svg";
+export { ReactComponent as Arrow } from "./arrow.svg";
