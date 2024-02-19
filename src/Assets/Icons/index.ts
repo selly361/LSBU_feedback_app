9f6c384db1d11f27bbfd7f17c2baf4ea5f5cb527
selly361 <seikou615@gmail.com>
2024-02-19 12:51:47 +0000

    Update: rename `SuggestionsIcon` to `FeedbacksIcon` and changed path

--- src/Assets/Icons/index.ts
+++ src/Assets/Icons/index.ts
@@ -4,7 +4,7 @@ export { ReactComponent as ArrowUpIcon } from "./icon-arrow-up.svg";
 export { ReactComponent as CheckIcon } from "./icon-check.svg";
 export { ReactComponent as CommentsIcon } from "./icon-comments.svg";
 export { ReactComponent as NewFeedbackIcon } from "./icon-new-feedback.svg";
-export { ReactComponent as SuggestionsIcon } from "./icon-suggestions.svg";
+export { ReactComponent as FeedbacksIcon } from "./icon-feedbacks.svg";
 export { ReactComponent as EmptyIllustration } from "./illustration-empty.svg";
 export { ReactComponent as ConfusedFace } from "./confused-face.svg";
 export { ReactComponent as Arrow } from "./arrow.svg";
