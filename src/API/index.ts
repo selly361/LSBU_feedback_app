db5fb0cf1bb4320069b8cfcf01215e92a90ae66d
Seikou <104414846+selly361@users.noreply.github.com>
2024-02-20 15:22:02 +0000

    Update: add `API_URL` using env

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,6 +1,6 @@
 import { Comment, Feedback, Reply, Tutorial } from 'Types'
 
-const API_URL = 'http://localhost:3001'
+const API_URL = process.env.REACT_APP_API_URL
 
 const fetchFeedbacks = async (): Promise<Feedback[]> => {
 	try {
