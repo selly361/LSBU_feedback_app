7157a117c8bf860cbdfbd556e6986dc94b71c282
Seikou <104414846+selly361@users.noreply.github.com>
2024-02-20 14:24:28 +0000

    Add: `index.ts` file to export pages

---
+++ src/Pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './Home'
+export { default as NewFeedback } from './NewFeedback'
+export { default as PageNotFound } from './PageNotFound'
+export { default as FeedbackDetail } from './FeedbackDetail'
