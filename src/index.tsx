b5ccbe7a9f4f3fa10215ed0596042f48d4947c6a
Seikou <104414846+selly361@users.noreply.github.com>
2024-02-20 14:06:49 +0000

    Update: wrap `App` with context providers and `BrowserRouter`

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+import { FeedbackProvider, FilterFeedbackProvider } from 'Contexts'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<FeedbackProvider>
+				<FilterFeedbackProvider>
+					<App />
+				</FilterFeedbackProvider>
+			</FeedbackProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 )
 
