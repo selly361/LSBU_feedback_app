d6f2da96ca6bf12519011e5f97fae43f01ef51da
Seikou <104414846+selly361@users.noreply.github.com>
2024-02-20 14:42:59 +0000

    Update index.tsx

--- src/Components/NoFeedbacksMessage/index.tsx
+++ src/Components/NoFeedbacksMessage/index.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 function NoFeedbacksMessage() {
 	return (
-		<div className='no-feedback-message'>
+		<div className='no-feedbacks-message'>
 			<EmptyIllustration />
-			<article className='no-feedback-message__article'>
-				<h3 className='no-feedback-message__article__title'>There is no feedback yet.</h3>
-				<p className='no-feedback-message__article__desc'>
+			<article className='no-feedbacks-message__article'>
+				<h3 className='no-feedbacks-message__article__title'>There is no feedback yet.</h3>
+				<p className='no-feedbacks-message__article__desc'>
 					Got a suggestion? Found a bug that needs to be squashed? We love
 					hearing about new ideas to improve our app.
 				</p>
