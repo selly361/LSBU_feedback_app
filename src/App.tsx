d939c80b894523f1cafb28fda9620103faf9084e
Seikou <104414846+selly361@users.noreply.github.com>
2024-02-20 14:24:14 +0000

    Add: initialize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { FeedbackDetail, Home, NewFeedback, PageNotFound } from 'Pages'
+import { Fragment } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-	return null
+	return (
+		<Fragment>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/new' element={<NewFeedback />} />
+				<Route path='/feedback/:id' element={<FeedbackDetail />} />
+				<Route path='*' element={<PageNotFound />} />
+			</Routes>
+		</Fragment>
+	)
 }
 
 export default App
