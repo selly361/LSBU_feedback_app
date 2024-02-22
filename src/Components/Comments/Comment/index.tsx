3f03e9124cd957200a6243846d08c7c59ddb3d73
selly361 <seikou615@gmail.com>
2024-02-22 14:51:52 +0000

    Add: `Comment` component

---
+++ src/Components/Comments/Comment/index.tsx
@@ -0,0 +1,28 @@
+import { Comment as IComment } from 'Types'
+import { useState } from "react";
+import useUniqueUsername from "Hooks/useUniqueUsername";
+import { PersonIcon } from 'Assets/Icons';
+import AddReply from '../AddReply';
+
+  
+  function Comment({ _id, text, username: commentsUsername }: IComment) {
+    const [reply, setReply] = useState(false)
+
+    return (
+      <section className={`comments__comment`}>
+        <PersonIcon />
+        <div className='comments__comment__container'>
+          <div className='comments__comment__container__sub-container'>
+            <h4 className='comments__username'>{commentsUsername}</h4>
+          </div>
+          <button className='comments__reply-button' onClick={() => setReply((e) => !e)}>Reply</button>
+          <p className='comments__text'>
+            {text}
+          </p>
+          {reply ? <AddReply closeReply={() => setReply(false)} commentId={_id as string} replyingTo={commentsUsername} /> : null}
+        </div>
+      </section>
+    )
+  }
+  
+  export default Comment
