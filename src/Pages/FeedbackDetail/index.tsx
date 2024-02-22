9276d77381cd73b4219557b8aae1521ce86fa731
selly361 <seikou615@gmail.com>
2024-02-22 14:53:06 +0000

    Uodate: refactored `FeedbackDetail` page

--- src/Pages/FeedbackDetail/index.tsx
+++ src/Pages/FeedbackDetail/index.tsx
@@ -1,14 +1,34 @@
-import { useParams } from 'react-router'
+import { useFeedbackContext } from 'Contexts' 
+import PageNotFound from 'Pages/PageNotFound' 
+import { useParams } from 'react-router' 
+import { useLocation } from 'react-router-dom' 
+import { AddComment, BackButton, Comments } from 'Components' 
+import Feedback from 'Components/Feedbacks/Feedback' 
 
-function FeedbackDetail() {
-
-    const { id } = useParams()
-
+function FeedbackDetail() { 
+  const { id } = useParams() 
+  const location = useLocation() 
+  const { feedbacks } = useFeedbackContext() 
+  const indexOfFeedback = feedbacks.findIndex(feedback => feedback._id === id)
+  
+  if(indexOfFeedback === -1){ 
+    return <PageNotFound /> 
+  } 
+  
+  const feedbackData = feedbacks[indexOfFeedback]
+  
   return (
-    <main className='main--feedback-detail'>
-        {id}
-    </main>
-  )
-}
+  <main className='main--feedback-detail'> 
+      <BackButton /> 
+      <Feedback {...feedbackData} /> 
+      <Comments comments={feedbackData.comments} />
+      <AddComment />
+  </main> 
+  ) 
+
+
+} 
+
+
 
 export default FeedbackDetail
