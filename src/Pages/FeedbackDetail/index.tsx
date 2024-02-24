254ce01367e4cb67691231db7944ab0ae50821f5
selly361 <seikou615@gmail.com>
2024-02-24 11:43:43 +0000

    Update: add loading when user refreshes page

--- src/Pages/FeedbackDetail/index.tsx
+++ src/Pages/FeedbackDetail/index.tsx
@@ -2,18 +2,24 @@ import { useFeedbackContext } from 'Contexts'
 import PageNotFound from 'Pages/PageNotFound' 
 import { useParams } from 'react-router' 
 import { useLocation } from 'react-router-dom' 
-import { AddComment, BackButton, Comments } from 'Components' 
+import { AddComment, BackButton, Comments, Loading } from 'Components' 
 import Feedback from 'Components/Feedbacks/Feedback' 
 
 function FeedbackDetail() { 
   const { id } = useParams() 
-  const location = useLocation() 
-  const { feedbacks } = useFeedbackContext() 
+  const { feedbacks, isLoading } = useFeedbackContext() 
   const indexOfFeedback = feedbacks.findIndex(feedback => feedback._id === id)
+
   
-  if(indexOfFeedback === -1){ 
+  if(isLoading){
+    return <main className='main--home'><Loading /></main>
+  }
+
+
+  else if(indexOfFeedback === -1 && !isLoading){ 
     return <PageNotFound /> 
   } 
+
   
   const feedbackData = feedbacks[indexOfFeedback]
   
