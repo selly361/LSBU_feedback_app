d3eb786d8274dab3d52d2ea2bec989bc032565f4
selly361 <seikou615@gmail.com>
2024-02-24 12:40:06 +0000

    Update: add loading

--- src/Pages/NewFeedback/index.tsx
+++ src/Pages/NewFeedback/index.tsx
@@ -1,7 +1,19 @@
-import { BackButton, Form } from 'Components'
+import { BackButton, Form, Loading } from 'Components'
+import { useFeedbackContext } from 'Contexts'
 
 
 function NewFeedback() {
+
+  const { isLoading } = useFeedbackContext()
+
+  if(isLoading) {
+    return (
+      <main className='main--home'>
+        <Loading />
+      </main>
+    )
+  }
+
   return (
     <main className='main--new-feedback'>
         <BackButton />
