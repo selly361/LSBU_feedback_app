240af17e56086e4886bc96171050c049fb164342
Seikou <104414846+selly361@users.noreply.github.com>
2024-03-05 08:45:56 +0000

    Update index.tsx

--- src/Pages/PageNotFound/index.tsx
+++ src/Pages/PageNotFound/index.tsx
@@ -1,14 +1,13 @@
 import { Arrow, ConfusedFace } from 'Assets/Icons'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function PageNotFound() {
+	const navigate = useNavigate()
 	return (
 		<main className='main--page-not-found'>
 			<h1 className='main--page-not-found__title'>404</h1>
 			<ConfusedFace />
-			<Link className='main--page-not-found__return-home-link' to='/'>
-				Go Back
-			</Link>
+			<button className='button--cancel' onClick={() => navigate(-1)}>Cancel</button>
 		</main>
 	)
 }
